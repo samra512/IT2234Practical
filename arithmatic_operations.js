7a9d5e0cb3d4f34dc2e84b7dddac1c12405417c2
samra512 <saja090331@gmail.com>
2025-03-12 01:28:27 +0530

    Add files via upload

---
+++ arithmatic_operations.js
@@ -0,0 +1,14 @@
+//aritmatic opertaions
+
+let x=5;
+let y=8;
+
+var num1=2;
+var num2=4;
+
+const i=1;
+
+console.log("x+y:" +(x+y))
+console.log("x-y:" +(x-y))
+console.log("x*y:" +(x*y))
+console.log("x/y:" +(x/y))
