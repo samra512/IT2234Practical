1ec8201fd41b1369d6fbb0ca54f7307d6e8db0d3
samra512 <saja090331@gmail.com>
2025-03-17 23:50:17 +0530

    Add files via upload

---
+++ array_operations.js
@@ -0,0 +1,12 @@
+
+let arr = ['a','b','c','d']
+arr.push('e')
+console.log(arr)
+console.log(" ")
+//reverse the array using push pop
+while(arr.length > 0)
+{
+	console.log(arr.pop())
+}
+
+console.log(" ") 
